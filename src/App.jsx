efe57d085b68573615644481d355c35c1a84bfea
nigolf <55658244+nigolf@users.noreply.github.com>
2021-08-15 18:16:47 +0900

    useEffectを使って再レンダリング

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaseShowFlag] = useState(false);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
 
   const onClickCountUp = () => {
     setNum(num + 1);
   };
 
   const onClickSwitchShowFlag = () => {
-    setFaseShowFlag(!faceShowFlag);
+    setFaceShowFlag(!faceShowFlag);
   };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
 
   return (
     <>
